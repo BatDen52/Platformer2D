51c1d85103f71a8c56e28006b9765b1de21112ce
BatDen52 <kaluzhin.denis@gmail.com>
2024-04-28 01:08:16 +0300

    init

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMover : MonoBehaviour
+{
+    private const float SPEED_COEFFICIENT = 50;
+
+    [SerializeField] private float _speedX = 1;
+    [SerializeField] private float _jumpForce = 500;
+
+    private Rigidbody2D _rigidbody;
+    private bool _isTurnRight = true;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Jump()
+    {
+        _rigidbody.AddForce(new Vector2(0, _jumpForce));
+    }
+
+    public void Move(float direction)
+    {
+        _rigidbody.velocity = new Vector2(_speedX * direction * SPEED_COEFFICIENT * Time.fixedDeltaTime, _rigidbody.velocity.y);
+
+        if ((direction > 0 && _isTurnRight == false)
+            || (direction < 0 && _isTurnRight))
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        _isTurnRight = !_isTurnRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
